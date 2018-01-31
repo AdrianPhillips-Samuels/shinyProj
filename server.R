5c45647aa5ad39b80cb846dcf0dfd65b0f021bd8
Adrian <adrian.phillips.samuels@gmail.com>
2018-01-30 23:37:09 -0500

    Updated

--- server.R
+++ server.R
@@ -4,16 +4,67 @@
 
 function(input, output, session) {
 
-  
+ # chartData = reactive({SPxts})
   
   output$candle = renderDygraph({
-    dygraph(SPxts) %>%
+    dygraph(SPxts,group = 'Main') %>%
       dyCandlestick(compress = TRUE) %>% 
-      dyRangeSelector(dateWindow = input$dateRange)
-    
+      dyRangeSelector()
   })
-#  observe({
-#    output$from = candle$date_window[[1]]
-#    output$to = candle$date_window[[2]]
-#  })
+  
+  
+
+  
+  output$vol = renderDygraph({
+    dygraph(SPVxts, group = 'Main') %>%
+      dyOptions(fillGraph = TRUE,maxNumberWidth = 20)
+  })
+
+      
+  output$from <- renderText({
+    if (!is.null(input$candle_date_window))
+      strftime(input$candle_date_window[[1]], "%d %b %Y")
+  })
+  output$to <- renderText({
+    if (!is.null(input$candle_date_window))
+      strftime(input$candle_date_window[[2]], "%d %b %Y")
+  })
+
+observeEvent(input$calcMA,{
+
+
+
+ifelse(input$MA1type=='Simple',
+       (SP500 = mutate(SP500, mov1 = SMA(x = SP500$Close,n = input$MA1))),
+       (SP500 = mutate(SP500, mov1 = EMA(x = SP500$Close,n = input$MA1)))
+)
+ifelse(input$MA2type=='Simple',
+       (SP500 = mutate(SP500, mov2 = SMA(x = SP500$Close,n = input$MA2))),
+       (SP500 = mutate(SP500, mov2 = EMA(x = SP500$Close,n = input$MA2)))
+)
+#                      SMA(x = SP500$Close,n = input$MA1),
+#                      EMA(x = SP500$Close,n = input$MA1)),
+#         mov2 = ifelse(input$MA2type=='Simple',
+#                      SMA(x = SP500$Close,n = input$MA2),
+#                      EMA(x = SP500$Close,n = input$MA2))         
+#         )
+ SPxts2 = xts(SP500[,c('Open','High','Low','Close','mov1','mov2')], order.by = SP500$Date)
+
+ output$candle = renderDygraph({
+   dygraph(SPxts2,group = 'Main') %>%
+     dyCandlestick() %>% 
+     dyRangeSelector()
+ })
+
+  
+})
+
+observeEvent(input$clearMA,{
+  SP500 = fread(file = "./GSPC.csv")
+  names(SP500)[names(SP500) == 'Adj Close'] = 'AdjClose'
+  SP500$Volume = as.numeric(SP500$Volume)
+  SP500$Date = as.Date(SP500$Date,"%m/%d/%Y")
+  SPxts = xts(SP500[,c('Open','High','Low','Close')], order.by = SP500$Date)
+})  
+  
 }
--- ui.R
+++ ui.R
@@ -1,23 +1,74 @@
 
 
 fluidPage(
-titlePanel("S&P 500"),
+titlePanel(h1("S&P 500")),
   
-  sidebarLayout(
-    sidebarPanel(
-      dateRangeInput("dateRange", label = h3("DateRange"),
-                     start = min(index(SPxts)), 
-                     end = max(index(SPxts)), 
-                     min = min(index(SPxts)), 
-                     max = max(index(SPxts))
-                     )
-    ),
-    mainPanel()
-  ),
+#  sidebarLayout(
+#    sidebarPanel(
+#      div(strong("From: "), textOutput("from", inline = TRUE)),
+#      div(strong("To: "), textOutput("to", inline = TRUE))
+#    ),
+    mainPanel(
+      
+      div(strong("From: "), textOutput("from", inline = TRUE)),
+      splitLayout(div(strong("To: "), textOutput("to", inline = TRUE)),
+                  div(checkboxInput('volToggle', 'Display Volume'),align = 'right')
+              )
+                    ),
+#  ),
   
   
   mainPanel(
-    dygraphOutput("candle")
-  )
+    
 
+    dygraphOutput("candle"),
+
+    
+    
+    
+    h1(textOutput("volTag")),
+    conditionalPanel(condition = 'input.volToggle', 
+      dygraphOutput("vol",height = "200px")
+                   ),
+#    div(style="display:inline-block",numericInput("MA1", "First Moving Average", value = 10, min = 1, max = 99, width = '120px')),
+#        div(style="display:inline-block",numericInput("MA2", "Second Moving Average", value = 20, min = 1, max = 99, width = '120px'))
+
+  #h2("Indicators"),
+  tabsetPanel(
+    tabPanel("Indicators",  
+  radioButtons("Indicators", "Select Indicators:",
+               choices = c('None','Moving Averages'#,'MACD'#, 'Ind3','Ind4', 'Ind5'
+               ), inline = TRUE),
+  
+
+
+    conditionalPanel("input.Indicators=='Moving Averages'",
+      splitLayout(
+        numericInput("MA1", "Short Moving Average (Days)", value = 10, min = 1, max = 99),
+
+        numericInput("MA2", "Long Moving Average(Days)", value = 20, min = 1, max = 99)
+      ),
+      splitLayout(
+        radioButtons('MA1type',choices = c('Simple','Exponential'),label = NULL,inline = TRUE),
+        radioButtons('MA2type',choices = c('Simple','Exponential'),label = NULL,inline = TRUE)
+
+      ),
+      splitLayout(
+        actionButton("calcMA",'Calculate'),
+        actionButton("clearMA",'Clear')
+      )
+    ),
+  br(),
+  br(),
+  br()
+    ),
+  tabPanel("Backtesting",
+           br(),
+           br(),
+           br(),
+           br()
+           
+           )
+  )
+  )
 )
